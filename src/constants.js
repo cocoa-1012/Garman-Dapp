0012101271b9b5d7f14400605a05e32c85537c23
cocoa-1012 <cocoa.pandaok@gmail.com>
2022-05-09 10:40:08 -0400

    manifest page update

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const manifestoText1 =
+  "Words spoken by survivors. Words spoken by those who fight for the light and the protection of Eden. The ones that have been cast away to he shadows, forgotten warriors. We are The Tarnished. The forgotten warriors of Eden, protectors of souls. We fight to bring the light back into those who have been led astray.";
+export const manifestoText2 =
+  "Once a peaceful and prosperous place, Eden was like no other. Until the darkness overtook the land, claiming the souls of those who lost their way. Now we must stand together as one to stop the darkness and bring light back into the forgotten souls.";
+export const manifestoText3 =
+  "Together we are strong, united we fight to defeat the dark and unleash the light, together we are The Tarnished. No longer shall we wait in the shadows. A new day is coming. The age of The Tarnished. Together we are stronger, together we remove doubt. Together we remove all boundaries.";
--- src/pages/ManifestoPage.jsx
+++ src/pages/ManifestoPage.jsx
@@ -1,8 +1,25 @@
+import mani_02 from "../assets/images/mani-02.png";
+import mani_03 from "../assets/images/mani-03.png";
 import manifesto from "../assets/images/manifesto.png";
+import { manifestoText1, manifestoText2, manifestoText3 } from "../constants";
 export const ManifestoPage = () => {
+  const maniTextClass = "text-white text-2xl my-8";
   return (
-    <div className="flex flex-col z-10 mt-40">
-      <img src={manifesto} alt="manifesto title" />
+    <div className="flex flex-col w-screen  max-w-screen-2xl z-10 mt-40">
+      <img className="m-auto" src={manifesto} alt="manifesto title" />
+      <div className="flex flex-row-reverse">
+        <img className="flex-end" src={mani_02} alt="mani-02" />
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="w-2/3">
+          <p className={maniTextClass}>{manifestoText1}</p>
+          <p className={maniTextClass}>{manifestoText2}</p>
+          <p className={maniTextClass}>{manifestoText3}</p>
+        </div>
+        <div className="bg-[] w-1/3">
+          <img className="h-screen" src={mani_03} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
