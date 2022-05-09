202d17ed378527884bc563d6cd38c9eee0b3898b
cocoa-1012 <cocoa.pandaok@gmail.com>
2022-05-09 10:57:48 -0400

    add prologue

--- src/pages/ManifestoPage.jsx
+++ src/pages/ManifestoPage.jsx
@@ -1,6 +1,8 @@
 import mani_02 from "../assets/images/mani-02.png";
 import mani_03 from "../assets/images/mani-03.png";
 import manifesto from "../assets/images/manifesto.png";
+import prologue from "../assets/images/prologue.png";
+import prologue2 from "../assets/images/prologue_2.png";
 import { manifestoText1, manifestoText2, manifestoText3 } from "../constants";
 export const ManifestoPage = () => {
   const maniTextClass = "text-white text-2xl my-8";
@@ -16,9 +18,15 @@ export const ManifestoPage = () => {
           <p className={maniTextClass}>{manifestoText2}</p>
           <p className={maniTextClass}>{manifestoText3}</p>
         </div>
-        <div className="bg-[] w-1/3">
+        <div className="w-1/3">
           <img className="h-screen" src={mani_03} alt="" />
         </div>
+      </div>
+      <div>
+        <img src={prologue} alt="prologue" />
+      </div>
+      <div className="flex flex-row-reverse -mt-60">
+        <img className="w-2/3" src={prologue2} alt="" />
       </div>
     </div>
   );
